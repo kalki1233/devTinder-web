d52c2e2a90b4276f2a54f2f46e8cb007a6669c09
Kalki <kalki.1436@gmail.com>
2025-09-03 01:56:41 +0530

    Create basic routes

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <footer className="footer footer-center bg-base-300 text-base-content p-4 fixed bottom-0">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by Kalki
+          Nallapati
+        </p>
+      </aside>
+    </footer>
+  );
+};
